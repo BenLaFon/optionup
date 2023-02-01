81c8fd2286cb6c73598d833d119dd073df048726
Benjamin LaFon <benjamin@ekohe.com>
2023-02-01 02:02:55 -0600

    got docker working

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ruby:3.2.0-alpine
+
+RUN apk add --update --virtual \
+    runtime-deps\
+    postgresql-client \
+    build-base \
+    libxml2-dev \
+    libxslt-dev \
+    nodejs \
+    yarn \
+    libffi-dev \
+    readline \
+    build-base \
+    postgresql-dev \
+    libc-dev \
+    linux-headers \
+    readline-dev \
+    file \
+    git \
+    tzdata \
+    gcompat \
+    && rm -rf /var/cache/apk/*
+
+WORKDIR /app
+COPY . /app
+ENV BUNDLE_PATH /gems
+RUN yarn install --check-files
+RUN bundle install
+
+ENTRYPOINT [ "bin/rails" ]
+CMD [ "s", "-b", "0.0.0.0" ]
+
+EXPOSE 3000
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,16 +1,16 @@
 # env :GEM_PATH, ENV['GEM_PATH']
+# ENV.each { |k, v| env(k, v) }
+# set :output, "cron_log.log"
+# set :bundle_command, "/Users/ben/.rbenv/shims/bundle exec"
+every 1.minute do
+  rake "test:test"
+end
 
-set :output, "cron_log.log"
-set :bundle_command, "/Users/ben/.rbenv/shims/bundle exec"
-# every 1.minute do
-#   rake "test:test"
-# end
-#
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
 
 # Learn more: http://github.com/javan/whenever
-every 1.day, at: '4:30 am' do
-  rake "update:records"
-end
+# every 1.day, at: '4:30 am' do
+#   rake "update:records"
+# end
